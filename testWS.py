417dbc1e8342394faa58534e4e65571a3b0a7de5
pganson <pganson@umich.edu>
2015-01-25 19:13:09 -0500

    use argparse to capture command-line arguments and pass them to closings().  added debugging 'print' statements (along with --verbose command-line flag) which write to stderr

--- testWS.py
+++ testWS.py
@@ -2,10 +2,12 @@
 from urllib import urlopen
 import re
 from itertools import chain
+import argparse
+import sys
 
 javascript = re.compile(r"ibsys.htvClosings.init\(({.*?})\)") #The closings are embeded in a javascript function. This will extract the dictionary to group 1.
 wgalAddress = "http://www.wgal.com/weather/closings"
-def closings(useCache=True, cache="closings.html"):
+def closings(useCache=True, cache="closings.html", verbose=False):
     """Returns a dictionary containing information about closed schools and bussinesses.
     Keyword Arguments:
     useCache -- If true function will use cache instead of going online (default True)
@@ -13,6 +15,8 @@ def closings(useCache=True, cache="closings.html"):
     """
     if not cache: useCache = False
     if useCache:
+        if verbose:
+            print >> sys.stderr, "DEBUG - reading HTML from %s, not Interweb" % cache
         try:
             with open(cache, 'r') as cacheFile:
                 wgalSite = cacheFile.read()
@@ -30,7 +34,17 @@ def closings(useCache=True, cache="closings.html"):
     return organizedDict
 
 if __name__ == '__main__':
-    for school, data in closings(False).iteritems():
+    parser = argparse.ArgumentParser(description='Process closing information from external website')
+    parser.add_argument('--usecache', dest='usecache', action='store_true', default=False, help='use the HTML cached on the local machine')
+    parser.add_argument('--verbose', dest='verbose', action='store_true', default=False, help='write debugging to stderr')
+    parser.add_argument('--cache', dest='cache', action='store', default="closings.html", type=str, help='name of the cache file')
+
+    args = parser.parse_args()
+
+    if args.verbose:
+        print  >> sys.stderr, "DEBUG - command-line argument(s), usecache=%s" % args.usecache
+
+    for school, data in closings(args.usecache,cache=args.cache,verbose=args.verbose).iteritems():
         print school
         for key, value in data.iteritems():
             print '\t'+key.upper()+': '+value
